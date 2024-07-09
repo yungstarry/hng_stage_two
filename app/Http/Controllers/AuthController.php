a619a69c20a08a66e543d8be1df5c53eed9afc04
yungstarry <111007711+yungstarry@users.noreply.github.com>
2024-07-09 22:35:17 +0100

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
 {
     public function signup(SignupRequest $request)
     {
-        $data = $request->validated();
+        $data = $request;
 
         try {
             $user = User::create([
